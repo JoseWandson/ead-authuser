f7a8fc60a61173d594f01e975ad38b132483d979
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2024-04-02 11:26:33 -0300

    Implementação do Retry Resilience4j e Configuração do Timeout

--- src/main/java/com/ead/authuser/clients/CourseClient.java
+++ src/main/java/com/ead/authuser/clients/CourseClient.java
@@ -3,11 +3,13 @@ package com.ead.authuser.clients;
 import com.ead.authuser.dtos.CourseDto;
 import com.ead.authuser.dtos.ResponsePageDto;
 import com.ead.authuser.services.UtilsService;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -31,6 +33,7 @@ public class CourseClient {
     @Value("${ead.api.url.course}")
     private String requestUrlCourse;
 
+    @Retry(name = "retryInstance", fallbackMethod = "retryfallback")
     public Page<CourseDto> getAllCoursesByUser(UUID userId, Pageable pageable) {
         ResponsePageDto<CourseDto> body = null;
         String url = requestUrlCourse + utilsService.createUrlGetAllCoursesByUser(userId, pageable);
@@ -53,5 +56,11 @@ public class CourseClient {
         log.info("Ending request /courses userId {}", userId);
 
         return body;
+    }
+
+    @SuppressWarnings("unused")
+    private Page<CourseDto> retryfallback(UUID userId, Pageable pageable, Throwable t) {
+        log.error("Inside retry retryfallback, cause - {}", t.toString());
+        return new PageImpl<>(new ArrayList<>());
     }
 }
